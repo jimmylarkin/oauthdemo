122162840b0df9f70a2e0c232d9cca14fe7510b3
Jimmy Larkin <jimmy.larkin@outlook.com>
2013-05-31 23:25:45 +0100

    added websecurity setup

---
+++ oauthdemo/App_Start/WebSecurityConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebMatrix.WebData;
+
+namespace oauthdemo
+{
+  public class WebSecurityConfig
+  {
+    public static void RegisterWebSecurityProvider()
+    {
+      WebSecurity.InitializeDatabaseConnection("DefaultConnection", "users", "id", "name", true);
+    }
+  }
+}
