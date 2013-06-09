00793f963ba95b9a5bfd56848d12d76be6a605e6
Jimmy Larkin <jimmy.larkin@outlook.com>
2013-06-09 23:12:17 +0100

    almost done with boostrap

--- oauthdemo/Models/AccountModels.cs
+++ oauthdemo/Models/AccountModels.cs
@@ -57,11 +57,11 @@ namespace oauthdemo.Models
 
   public class LoginModel
   {
-    [Required]
+    [Required(ErrorMessage = "The username is required")]
     [Display(Name = "User name")]
     public string UserName { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "The password is required")]
     [DataType(DataType.Password)]
     [Display(Name = "Password")]
     public string Password { get; set; }
@@ -72,11 +72,11 @@ namespace oauthdemo.Models
 
   public class RegisterModel
   {
-    [Required]
+    [Required(ErrorMessage = "The username is required")]
     [Display(Name = "User name")]
     public string UserName { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "The password is required")]
     [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
     [DataType(DataType.Password)]
     [Display(Name = "Password")]
