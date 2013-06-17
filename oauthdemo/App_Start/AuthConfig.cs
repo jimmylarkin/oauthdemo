5b04450ffe0b64823f267ec79773b98b2cefc02b
Jimmy Larkin <jimmy.larkin@outlook.com>
2013-06-17 23:09:47 +0100

    all clients working

--- oauthdemo/App_Start/AuthConfig.cs
+++ oauthdemo/App_Start/AuthConfig.cs
@@ -18,15 +18,15 @@ namespace oauthdemo
           clientId: "00000000400F8C05",
           clientSecret: "bll3xF4lvpaWQdgrtgL7BP6VpKkzXuD1");
 
-      //OAuthWebSecurity.RegisterTwitterClient(
-      //    consumerKey: "",
-      //    consumerSecret: "");
+      OAuthWebSecurity.RegisterTwitterClient(
+          consumerKey: "Z13QcmG1IPgfIyNH4Q84Zg",
+          consumerSecret: "vF1NO5EWbULRRV6Hxnl9NVV86tdPloq8uNDfGdxfA");
 
-      //OAuthWebSecurity.RegisterFacebookClient(
-      //    appId: "",
-      //    appSecret: "");
+      OAuthWebSecurity.RegisterFacebookClient(
+          appId: "153997748117499",
+          appSecret: "1bc14a00d95031a38cbf688f87de61a2");
 
-      //OAuthWebSecurity.RegisterGoogleClient();
+      OAuthWebSecurity.RegisterGoogleClient();
     }
   }
 }
