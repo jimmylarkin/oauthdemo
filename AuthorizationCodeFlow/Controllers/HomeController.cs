503496a473c8b7c9d44249dd8367666d25e9759f
Jimmy Larkin <jimmy.larkin@outlook.com>
2013-09-14 15:01:04 +0100

    authorization flow demo

---
+++ AuthorizationCodeFlow/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AuthorizationCodeFlow.Controllers
+{
+  public class HomeController : Controller
+  {
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Start()
+    {
+      string url = string.Format("https://login.live.com/oauth20_authorize.srf?client_id={0}&scope={1}&response_type=code&redirect_uri={2}",
+        "000000004810217E",
+        "wl.basic",
+        HttpUtility.UrlEncode("http://demo.my/Home/Response"));
+      return Redirect(url);
+    }
+
+    public ActionResult Response(string code)
+    {
+      WebClient client = new WebClient();
+      NameValueCollection form = new NameValueCollection();
+      form.Add("client_id", "000000004810217E");
+      form.Add("redirect_uri", "http://demo.my/Home/Response");
+      form.Add("client_secret", "ynlv5klzgQFjHy4sK3XXRdyYw-oA-Fev");
+      form.Add("code", code);
+      form.Add("grant_type", "authorization_code");
+
+      byte[] responseBytes = client.UploadValues("https://login.live.com/oauth20_token.srf", "POST", form);
+      string responseString = Encoding.ASCII.GetString(responseBytes);
+
+      var response = JsonConvert.DeserializeObject<OAuthTokenResponse>(responseString);
+      response.RawResponse = responseString;
+
+      ResponseViewModel model = new ResponseViewModel();
+      model.Code = code;
+      model.TokenResponse = response;
+
+      return View(model);
+    }
+  }
+
+  public class OAuthTokenResponse
+  {
+    [JsonIgnore]
+    public string RawResponse { get; set; }
+
+    [JsonProperty("token_type")]
+    public string TokenType { get; set; }
+
+    [JsonProperty("scope")]
+    public string Scope { get; set; }
+
+    [JsonProperty("access_token")]
+    public string Token { get; set; }
+
+    [JsonProperty("expires_in")]
+    public int Expiry { get; set; }
+  }
+
+  public class ResponseViewModel
+  {
+    public string Code { get; set; }
+    public OAuthTokenResponse TokenResponse { get; set; }
+  }
+}
---
+++ AuthorizationCodeFlow/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AuthorizationCodeFlow.MvcApplication" Language="C#" %>
